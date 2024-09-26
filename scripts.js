fc5e527c04c64929a6fac94a7f7f3becb8e5e199
Eunice Adu-Agyei <166177958+EAduAgyei@users.noreply.github.com>
2024-09-25 22:26:41 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -34,3 +34,38 @@ function handleSignUp(){
         alert('Please fill out all fields');
     }
 }
+
+function renderHomePage(){
+    rootDiv.innerHTML ='
+        <h1>Welcome, $(userName)!</h1>
+        <h2>Create a Post</h2>
+        <textarea id="postContent" placeholder="What's on your mind?"></textarea><br></br>
+        <button type="button" onclick="handleCreatePost()">Post</button>
+        <h3>Your Posts</h3>
+        <ul id="postList"></ul>
+    ';
+}
+
+let posts = [];
+
+function handleCreatePost(){
+    const postContent = document.getElementById('postContent').value;
+    if(postContent){
+        posts.push(postContent);
+        renderPostList();
+    }else{
+        alert('Post content cannot be empty');
+    }
+}
+
+function renderPostList(){
+cost postListElement = document.getElementById('postList');
+postListElement.innerHTML = '';
+
+post.forEach((post) => {
+    const postItem = document.createElement('li');
+    postItem.textContent = post;
+    postListElement.appendChild(postItem);
+    });
+}
+    
