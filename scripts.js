9df8e7630b0b00218116a934b62590e21ba9e5ff
Eunice Adu-Agyei <166177958+EAduAgyei@users.noreply.github.com>
2024-09-25 21:52:48 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,20 @@
 const rootDiv = document.getElementById('root');
+
+function renderSignUp(){
+
+    rootDiv.innerHTML =
+
+    <h1>Sign Up</h1>
+    <form id = "signupForm">
+        <label for="name">Name: </label>
+        <input type="text" id="name" placeholder="Enter your name"><br></br></input>
+        <label for="email">Email: </label>
+        <input type="email" id="email" placeholder="Enter your email"><br></br></input>
+        <label for="passsword">Password: </label>
+        <input type="password" id="password" placeholder="Enter your password"><br></br></input>
+        <button type="button" onclick="handleSignUp()">Sign Up</button>
+        
+    </form>
+    ;
+
+}
